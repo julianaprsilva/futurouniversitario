0bd9f5f94f2ac1b2476117dc2fbcd5b68081a8f3
julianaprsilva <julianapr.silva@gmail.com>
2015-12-13 22:46:43 -0200

    Inicio

---
+++ js/modules.js
@@ -0,0 +1,19 @@
+angular.module('FuturoUniversitario',  ['ngRoute', 'ngMessages']);
+
+
+/*angular.module('NotesApp').config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+      when('/lembrete', {
+        templateUrl: 'templates/lembrete.html',
+        controller: 'LembreteController'
+      }).
+      when('/tarefa', {
+        templateUrl: 'templates/tarefa.html',
+        controller: 'LembreteController'
+      }).
+      otherwise({
+        redirectTo: '/',
+        templateUrl: 'templates/lista.html',
+        controller: 'LembreteController'
+      });
+  }]);*/
